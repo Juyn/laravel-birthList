11e957b405212324b5d7aaebe9f277606cc79fd4
Xavier Dubois <juyn89@gmail.com>
2018-08-18 09:24:40 +0200

    fix typo

--- app/Category.php
+++ app/Category.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Category extends Model
 {
-    protected $table = 'Category';
+    protected $table = 'category';
     public $timestamps = true;
 
     /**
